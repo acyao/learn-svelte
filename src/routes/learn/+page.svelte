cc0824884e8b44b9c0b8bbfa683699172369662d
acyao <ac_yao94@hotmail.com>
2023-08-13 22:49:24 +0800

    first commit for svelte
    -create svelte demo project
    -add new page and play around textbox and button with data binding and if else statement and event

---
+++ src/routes/learn/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { text } from '@sveltejs/kit';
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+
+    let textbox1 = "";
+    let checkbox1 = false;
+
+    function subscribe(){
+        alert('Good Luck! HAHA! Sending email spam to ' + textbox1);
+    }
+</script>
+
+<p>Learn</p>
+
+<input style="width: 20%;" bind:value={textbox1} type="email"/>
+<p>{textbox1}</p>
+
+<label>
+    <input type="checkbox" bind:checked={checkbox1} />
+    Send me email spam!
+</label>
+
+{#if checkbox1}
+    <p>Nice! Your mailbox gonna blow up!</p>
+{:else}
+    <p>Please tick a checkbox!</p>
+{/if}
+
+<button disabled={!checkbox1} on:click={subscribe}>Subscribe</button>

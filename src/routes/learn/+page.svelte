4b65e6108257037df93ce803313520f3bc142537
acyao <ac_yao94@hotmail.com>
2023-08-23 20:52:27 +0800

    add progressive bar

--- src/routes/learn/+page.svelte
+++ src/routes/learn/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import { text } from '@sveltejs/kit';
     import type { PageData } from './$types';
+    import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+	import { page } from '$app/stores';
+	import { preprocess } from 'svelte/compiler';
+
     export let data: PageData;
 
     let textbox1 = "";
@@ -16,15 +21,23 @@
     $: count = textbox1.length * 2;
     $: checkBoxColorToggle = checkbox1 ? 'green' : 'red';
 
+    const progress = tweened(0, {
+        duration: 400,
+        easing: cubicOut
+    });
+
     function subscribe(){
         alert('Good Luck! HAHA! Sending email spam to ' + textbox1);
+        progress.set(1);
+        document.getElementById("text1").style.display = 'block';
+        
     }
 </script>
 
 <p>{word}</p>
-<img {src} alt={name} style="height: 50%; width:50%" />
+<img {src} alt={name} style="height: 50%; width:50%; margin:20px 0px" />
 
-<input style="width: 20%;" bind:value={textbox1} type="email"/>
+<input style="width: 40%;" bind:value={textbox1} type="email" placeholder="Enter Your Email"/>
 
 {#if textbox1.length > 0}
     <p>{count} - reactive declarations</p>
@@ -43,5 +56,9 @@
     <p style="color: {checkBoxColorToggle};">Please tick a checkbox!</p>
 {/if}
 
-<button disabled={!checkbox1} on:click={subscribe}>Subscribe</button>
+<button style="width: 40%;" disabled={!checkbox1} on:click={subscribe}>Subscribe</button>
+
+<progress style="width: 40%; margin: 20px 0px" value={$progress} />
+
+<p id="text1" style="display: none; font-size:30px;color:red">Thank you for let us collect and sell your data.</p>
 
